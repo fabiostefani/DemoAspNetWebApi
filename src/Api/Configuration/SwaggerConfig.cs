b90092624ffa7369692bca9f0563cc3d626a1dbd
Fabio de Stefani <fabiostefani@gmail.com>
2021-11-11 06:39:05 -0300

    Doc API

--- src/Api/Configuration/SwaggerConfig.cs
+++ src/Api/Configuration/SwaggerConfig.cs
@@ -14,21 +14,30 @@ namespace Api.Configuration
             {                
                 //c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My Api", Version = "v1" });
                 c.OperationFilter<SwaggerDefaultValues>();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
 
-    //             var security = new Dictionary<string, IEnumerable<string>>
-    //             {
-    //                 {"Bearer", new string[] { }}
-    //             };
-
-    //             c.AddSecurityDefinition("Bearer" ,new ApiKeyScheme
-    //             {
-    //                 Description = "Insira o token JWT desta maneira: Bearer {seu token}",
-    //                 Name = "Authorization",
-    //                 In = "header",
-    //                 Type = "apiKey"
-    //             });
-
-    //             c.AddSecurityRequirement(security);
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
 
             return services;
@@ -37,9 +46,9 @@ namespace Api.Configuration
         public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
         {
             
-    //         //app.UseMiddleware<SwaggerAuthorizedMiddleware>();
-             app.UseSwagger();
-             app.UseSwaggerUI(
+            //app.UseMiddleware<SwaggerAuthorizedMiddleware>();
+            app.UseSwagger();
+            app.UseSwaggerUI(
                 options =>
                 {
                     foreach (var description in provider.ApiVersionDescriptions)
@@ -125,25 +134,25 @@ namespace Api.Configuration
         }
     }
 
-    // public class SwaggerAuthorizedMiddleware
-    // {
-    //     private readonly RequestDelegate _next;
+    public class SwaggerAuthorizedMiddleware
+    {
+        private readonly RequestDelegate _next;
 
-    //     public SwaggerAuthorizedMiddleware(RequestDelegate next)
-    //     {
-    //         _next = next;
-    //     }
+        public SwaggerAuthorizedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-    //     public async Task Invoke(HttpContext context)
-    //     {
-    //         if (context.Request.Path.StartsWithSegments("/swagger")
-    //             && !context.User.Identity.IsAuthenticated)
-    //         {
-    //             context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-    //             return;
-    //         }
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments("/swagger")
+                && !context.User.Identity.IsAuthenticated)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                return;
+            }
 
-    //         await _next.Invoke(context);
-    //     }
-    // }
+            await _next.Invoke(context);
+        }
+    }
 }
